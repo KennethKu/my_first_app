5278594599a8eb9d09393b066e9c0d4fbb3b0e8e
Khye <kukhyenyin@gmail.com>
2021-12-02 07:37:43 +0800

    add angular component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Food } from './food';
+import { FoodService } from './food.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'my-first-app';
+export class AppComponent implements OnInit {
+  public foods: Food[] | undefined; 
+
+  constructor(private foodService: FoodService) { }
+
+  ngOnInit() {
+    this.getFoods();
+  }
+
+  public getFoods(): void {
+    this.foodService.getFoods().subscribe(
+      (response: Food[]) => {
+        this.foods = response;
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    )
+  }
 }
--- src/app/food.service.ts
+++ src/app/food.service.ts
@@ -1,13 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { Food } from './food';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FoodService {
-  private apiServerUrl = '';
+  private apiServerUrl = environment.apiBaseUrl;
 
   constructor(private http: HttpClient) { }
 
